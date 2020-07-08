655dc39c44e7df97128ab45377c8b41d822c3cd5
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-07-08 10:45:29 -0500

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -14,12 +26,11 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -60,3 +71,6 @@ var displayWarning = function(repo) {
 
     limitWarningEl.appendChild(linkEl);
 }
+
+getRepoName();
+
